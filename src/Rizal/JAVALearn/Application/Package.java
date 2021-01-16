fbffa7a112fc5a6563698214d8886fe96358f119
Safrizal <axrizal092@gmail.com>
2021-01-16 15:19:05 +0700

    Access Modifier and Import

---
+++ src/Rizal/JAVALearn/Application/Package.java
@@ -0,0 +1,22 @@
+package Rizal.JAVALearn.Application;
+
+
+import Rizal.JAVALearn.Data.Product;
+/*
+Import adalah kemampuan menggunakan class yang berbeda di package yang berbeda
+syaratnya yaitu class nya harus public
+ */
+
+
+
+public class Package {
+
+    public static void main(String[] args) {
+
+        Product product = new Product("Sepatu", 40);
+
+
+    }
+
+
+}
---
+++ src/Rizal/JAVALearn/Data/Product.java
@@ -0,0 +1,24 @@
+package Rizal.JAVALearn.Data;
+
+
+    /*
+        Access Modifier
+
+            Modifier     Class   Package     Subclass    World
+            public       Y       Y           Y           Y
+            protected    Y       Y           Y           N
+            no modifier  Y       Y           N           N
+            privat       Y       N           N           N
+        */
+
+public class Product {
+
+    String nama;
+    int size;
+
+    public Product(String name, int size){
+
+    }
+
+
+}
