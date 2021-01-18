2df0225e72cd6dc47f46ad962b1180f4ab0494cb
Safrizal <axrizal092@gmail.com>
2021-01-18 14:05:18 +0700

    toString

---
+++ src/Rizal/JAVALearn/ToString/Product.java
@@ -0,0 +1,18 @@
+package Rizal.JAVALearn.ToString;
+
+public class Product {
+
+    public String name;
+    public int price;
+
+    public Product(String name, int price){
+        this.name = name;
+        this.price = price;
+
+    }
+
+
+    public String toString(){
+        return "Product name: "+name+", Price: Rp "+price;
+    }
+}
---
+++ src/Rizal/JAVALearn/ToString/ProductApp.java
@@ -0,0 +1,10 @@
+package Rizal.JAVALearn.ToString;
+
+public class ProductApp {
+    public static void main(String[] args) {
+
+        var product= new Product("Mac Book", 30000000);
+
+        System.out.println(product);
+    }
+}
